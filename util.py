1d4dac7a06f6f96a4f902ba85285194753afb11e
arshadsaiifi <saifiarshad537@gmail.com>
2025-04-29 08:50:59 +0530

    Update util.py

--- util.py
+++ util.py
@@ -30,7 +30,7 @@ def load_saved_artifacts():
     global __data_columns
     global __locations
 
-    with open("./artifacts/columns.json", "r") as f:
+    with open("./columns.json", "r") as f:
         __data_columns = json.load(f)['data_columns']
         __locations = __data_columns[3:]
 
